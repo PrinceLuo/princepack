04f65bebb30a6390e3cff601eaf27d0ea09845bd
PrinceLuo <luoruichun1990@gmail.com>
2019-07-10 16:13:29 +0800

    Debug and synch to the Cloud

--- princesite/app/Http/Controllers/WxPublicPlatformController.php
+++ princesite/app/Http/Controllers/WxPublicPlatformController.php
@@ -9,10 +9,11 @@ class WxPublicPlatformController extends Controller
     //
     public function initCheck(Request $request){
         
+        $token = 'luo_test_token';
         $signature = $request->signature;
         $timestamp = $request->timestamp;
         $nonce = $request->nonce;
-        $tmpArr = array($timestamp,$nonce);
+        $tmpArr = array($timestamp,$nonce,$token);
         sort($tmpArr, SORT_STRING);
         $tmpStr = implode($tmpArr);
         $compressed_signature = sha1($tmpStr);
